d96c51594c61d938aadde32fa99d6e685391b974
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-02 10:42:02 -0600

    chore: Update file upload to include timestamp in filename

--- src/routes/upload_file.js
+++ src/routes/upload_file.js
@@ -11,15 +11,8 @@ router.use(express.json());
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     const rfc = req.body.rfc;
-    console.log('RFC en storage:', rfc);
-
-    if (!rfc) {
-      return cb(new Error('RFC is required'), null);
-    }
-
     const uploadPath = path.join('uploads', rfc);
 
-    // Crea el directorio si no existe
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
@@ -27,7 +20,8 @@ const storage = multer.diskStorage({
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    const rfc = req.body.rfc;
+    cb(null, `${rfc}-${Date.now()}${path.extname(file.originalname)}`);
   }
 });
 
@@ -35,7 +29,6 @@ const upload = multer({ storage: storage });
 
 const verifyRFC = (req, res, next) => {
   console.log('Verificando RFC:', req.body.rfc);
-  console.log('Request Body:', req.body);
   if (!req.body.rfc) {
     return res.status(400).send({ message: 'Please provide an RFC' });
   }
@@ -44,13 +37,12 @@ const verifyRFC = (req, res, next) => {
 
 router.post('/upload', upload.single('file'), verifyRFC, (req, res) => {
   console.log('Archivo recibido:', req.file);
-  console.log('Request Body después de subir el archivo:', req.body);
   if (!req.file) {
     return res.status(400).send({ message: 'Please upload a file' });
   }
 
   const rfc = req.body.rfc;
-  const filename = req.file.originalname;
+  const filename = req.file.filename;
   const filePath = path.join('uploads', rfc, filename);
 
   res.send({
